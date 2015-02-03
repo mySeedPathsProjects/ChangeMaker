89cb2c8c5abc7adf320208c5cd2030160bfec777
nate stephens <ndstephens@gmail.com>
2015-02-02 21:20:03 -0700

    finished project

--- ChangeMaker/ChangeMaker/Program.cs
+++ ChangeMaker/ChangeMaker/Program.cs
@@ -10,17 +10,97 @@ namespace ChangeMaker
     {
         static void Main(string[] args)
         {
-            //calling the function with $4.19.  
+            //calling the function ChangeAmount 
             //Notice that when using the decimal format you must end numbers with an 'm'
-            ChangeAmount(4.19m);
+            ChangeAmount(3.18m);
+            ChangeAmount(0.99m);
+            ChangeAmount(12.93m);
+
+            //keep console open
+            Console.ReadKey();
         }
 
+        /// <summary>
+        /// Calculate number of dollars and coins to produce same amount entered and print results.
+        /// </summary>
+        /// <param name="amount">amount in decimal format (number followed by an 'm')</param>
+        /// <returns>Change object for testing</returns>
         public static Change ChangeAmount(decimal amount) 
         {
+            //print original total amount before "amount" is brought to 0 with following code, with empty line space following
+            Console.WriteLine("AMOUNT: " + "$" + amount);
+            Console.WriteLine();
+            
             //this is our object that will hold the data of how many coins of each type to return
             Change amountAsChange = new Change();
            
-            //TODO: Fill in the the code to make this function work
+            //subtract as many quarters ($0.25) from "amount" until under 0.25 remaining, the repeat process with dimes, nickels, and pennies
+            while (amount >= 100.00m)
+            {
+                amount -= 100.00m;
+                amountAsChange.Hundreds++;
+            }
+            while (amount >= 50.00m)
+            {
+                amount -= 50.00m;
+                amountAsChange.Fifties++;
+            }
+            while (amount >= 20.00m)
+            {
+                amount -= 20.00m;
+                amountAsChange.Twenties++;
+            }
+            while (amount >= 10.00m)
+            {
+                amount -= 10.00m;
+                amountAsChange.Tens++;
+            }
+            while (amount >= 5.00m)
+            {
+                amount -= 5.00m;
+                amountAsChange.Fives++;
+            }
+            while (amount >= 1.00m)
+            {
+                amount -= 1.00m;
+                amountAsChange.Singles++;
+            }
+            while (amount >= 0.25m)
+            {
+                amount -= 0.25m;
+                amountAsChange.Quarters++;
+            }
+            while (amount >= 0.10m)
+            {
+                amount -= 0.10m;
+                amountAsChange.Dimes++;
+            }
+            while (amount >= 0.05m)
+            {
+                amount -= 0.05m;
+                amountAsChange.Nickels++;
+            }
+            while (amount > 0)
+            {
+                amount -= 0.01m;
+                amountAsChange.Pennies++;
+            }
+
+            //outputting to the console
+            Console.WriteLine("HUNDREDS: " + amountAsChange.Hundreds);
+            Console.WriteLine("FIFTIES: " + amountAsChange.Fifties);
+            Console.WriteLine("TWENTIES: " + amountAsChange.Twenties);
+            Console.WriteLine("TENS: " + amountAsChange.Tens);
+            Console.WriteLine("FIVES: " + amountAsChange.Fives);
+            Console.WriteLine("SINGLES: " + amountAsChange.Singles);
+            Console.WriteLine("QUARTERS: " + amountAsChange.Quarters);
+            Console.WriteLine("DIMES: " + amountAsChange.Dimes);
+            Console.WriteLine("NICKELS: " + amountAsChange.Nickels);
+            Console.WriteLine("PENNIES: " + amountAsChange.Pennies);
+
+            //two lines spaces added for readability of next ChangeAmount call
+            Console.WriteLine();
+            Console.WriteLine();
 
             //return our Change Object
             return amountAsChange;
@@ -48,9 +128,41 @@ namespace ChangeMaker
         }
 
     }
-
+    /// <summary>
+    /// Class "Change" to hold properties of Hundreds, Fifties, Twenties, Tens, Five, Singles Quarter, Dimes, Nickels, and Pennies when working with ChangeAmount function
+    /// </summary>
     public class Change
     {
+        /// <summary>
+        /// This is property to hold the number of Quarters to be returned as change
+        /// </summary>
+        public int Hundreds { get; set; }
+
+        /// <summary>
+        /// This is property to hold the number of Quarters to be returned as change
+        /// </summary>
+        public int Fifties { get; set; }
+
+        /// <summary>
+        /// This is property to hold the number of Quarters to be returned as change
+        /// </summary>
+        public int Twenties { get; set; }
+
+        /// <summary>
+        /// This is property to hold the number of Quarters to be returned as change
+        /// </summary>
+        public int Tens { get; set; }
+
+        /// <summary>
+        /// This is property to hold the number of Quarters to be returned as change
+        /// </summary>
+        public int Fives { get; set; }
+
+        /// <summary>
+        /// This is property to hold the number of Quarters to be returned as change
+        /// </summary>
+        public int Singles { get; set; }
+
         /// <summary>
         /// This is property to hold the number of Quarters to be returned as change
         /// </summary>
@@ -64,7 +176,7 @@ namespace ChangeMaker
         /// <summary>
         /// This is property to hold the number of Nickles to be returned as change
         /// </summary>
-        public int Nickles { get; set; }
+        public int Nickels { get; set; }
 
         /// <summary>
         /// This is property to hold the number of Pennies to be returned as change
@@ -76,9 +188,15 @@ namespace ChangeMaker
         /// </summary>
         public Change()
         {
+            this.Hundreds = 0;
+            this.Fifties = 0;
+            this.Twenties = 0;
+            this.Tens = 0;
+            this.Fives = 0;
+            this.Singles = 0;
             this.Quarters = 0;
             this.Dimes = 0;
-            this.Nickles = 0;
+            this.Nickels = 0;
             this.Pennies = 0;
         }
     }
--- ChangeMaker/ChangeMaker/Test.cs
+++ ChangeMaker/ChangeMaker/Test.cs
@@ -15,7 +15,7 @@ namespace ChangeMaker
             Change amountAsChange = Program.ChangeAmount(4.19m);
             Assert.IsTrue(amountAsChange.Quarters == 16, "Incorrect Quarters");
             Assert.IsTrue(amountAsChange.Dimes == 1, "Incorrect Dimes");
-            Assert.IsTrue(amountAsChange.Nickles == 1, "Incorrect Nickles");
+            Assert.IsTrue(amountAsChange.Nickels == 1, "Incorrect Nickles");
             Assert.IsTrue(amountAsChange.Pennies == 4, "Incorrect Pennies");
         }
         
@@ -25,7 +25,7 @@ namespace ChangeMaker
             Change amountAsChange = Program.ChangeAmount(3.18m);
             Assert.IsTrue(amountAsChange.Quarters == 12, "Incorrect Quarters");
             Assert.IsTrue(amountAsChange.Dimes == 1, "Incorrect Dimes");
-            Assert.IsTrue(amountAsChange.Nickles == 1, "Incorrect Nickles");
+            Assert.IsTrue(amountAsChange.Nickels == 1, "Incorrect Nickles");
             Assert.IsTrue(amountAsChange.Pennies == 3, "Incorrect Pennies");
         }
         [Test, Timeout(500)]
@@ -34,7 +34,7 @@ namespace ChangeMaker
             Change amountAsChange = Program.ChangeAmount(.99m);
             Assert.IsTrue(amountAsChange.Quarters == 3, "Incorrect Quarters");
             Assert.IsTrue(amountAsChange.Dimes == 2, "Incorrect Dimes");
-            Assert.IsTrue(amountAsChange.Nickles == 0, "Incorrect Nickles");
+            Assert.IsTrue(amountAsChange.Nickels == 0, "Incorrect Nickles");
             Assert.IsTrue(amountAsChange.Pennies == 4, "Incorrect Pennies");
         }
 
@@ -44,7 +44,7 @@ namespace ChangeMaker
             Change amountAsChange = Program.ChangeAmount(12.93m);
             Assert.IsTrue(amountAsChange.Quarters == 51, "Incorrect Quarters");
             Assert.IsTrue(amountAsChange.Dimes == 1, "Incorrect Dimes");
-            Assert.IsTrue(amountAsChange.Nickles == 1, "Incorrect Nickles");
+            Assert.IsTrue(amountAsChange.Nickels == 1, "Incorrect Nickles");
             Assert.IsTrue(amountAsChange.Pennies == 3, "Incorrect Pennies");
         }
 
